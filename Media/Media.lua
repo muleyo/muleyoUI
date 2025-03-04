e83f5cbb50b4cdb86ba82d9c41cd7989ad524829
muleyo <admin@muleyo.dev>
2025-03-05 00:00:34 +0100

    Updated Font Filename

--- Media/Media.lua
+++ Media/Media.lua
@@ -9,7 +9,7 @@ function Media:OnInitialize()
 
     -- Font
     Media.LSM:Register("font", "None", STANDARD_TEXT_FONT)
-    Media.LSM:Register("font", "Arial Bold", [[Interface\Addons\mUI\Media\Fonts\ArialBold.ttf]])
+    Media.LSM:Register("font", "Arial Bold", [[Interface\Addons\mUI\Media\Fonts\Arial_Bold.ttf]])
     Media.LSM:Register("font", "Avant Garde", [[Interface\Addons\mUI\Media\Fonts\AvantGarde.ttf]])
     Media.LSM:Register("font", "Doris P Bold", [[Interface\Addons\mUI\Media\Fonts\DorisPBold.ttf]])
     Media.LSM:Register("font", "Exo2 Bold", [[Interface\Addons\mUI\Media\Fonts\Exo2Bold.ttf]])
