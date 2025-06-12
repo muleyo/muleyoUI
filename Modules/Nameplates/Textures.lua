a2d2108c36a285d2b0c90ec0499963cb5abcf73d
muleyo <admin@muleyo.dev>
2025-06-12 04:17:29 +0200

    Added target arrows to nameplates

--- Modules/Nameplates/Textures.lua
+++ Modules/Nameplates/Textures.lua
@@ -13,8 +13,30 @@ function Textures:OnInitialize()
     -- Get Nameplates
     Textures.nameplates = {}
 
+    -- Target Arrows
+    Textures.arrows = CreateFrame("Frame", nil)
+    Textures.arrows.left = Textures.arrows:CreateTexture(nil, "OVERLAY")
+    Textures.arrows.left:SetTexture([[Interface\AddOns\mUI\Media\Textures\Nameplates\arrowLeft.png]])
+    Textures.arrows.left:SetSize(16, 16)
+    Textures.arrows.right = Textures.arrows:CreateTexture(nil, "OVERLAY")
+    Textures.arrows.right:SetTexture([[Interface\AddOns\mUI\Media\Textures\Nameplates\arrowRight.png]])
+    Textures.arrows.right:SetSize(16, 16)
+
     function Textures:SetTextures(nameplate)
         local texture = Textures.LSM:Fetch('statusbar', Textures.db.texture)
+
+        -- Target Arrows
+        if UnitExists("target") and UnitIsUnit(nameplate.unit, "target") then
+            Textures.arrows.left:SetPoint("LEFT", nameplate.healthBar, "LEFT", -16, 0)
+            Textures.arrows.right:SetPoint("RIGHT", nameplate.healthBar, "RIGHT", 16, 0)
+
+            Textures.arrows:Show()
+        elseif not UnitExists("target") then
+            Textures.arrows:Hide()
+        end
+
+        -- Hide Classification
+        nameplate.ClassificationFrame:SetAlpha(0)
 
         if nameplate.unit then
             if Textures.db.texture == "None" then
@@ -89,11 +111,12 @@ end
 
 function Textures:OnEnable()
     Textures.textures:RegisterEvent("PLAYER_FOCUS_CHANGED")
+    Textures.textures:RegisterEvent("PLAYER_TARGET_CHANGED")
     Textures.textures:RegisterEvent("NAME_PLATE_CREATED")
     Textures.textures:RegisterEvent("NAME_PLATE_UNIT_ADDED")
     --Textures.textures:RegisterEvent("NAME_PLATE_UNIT_REMOVED")
 
-    Textures:HookScript(Textures.textures, "OnEvent", function()
+    Textures:HookScript(Textures.textures, "OnEvent", function(_, event)
         Textures:RefreshNameplates()
     end)
 end
