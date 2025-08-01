b1d28a1421d6ad9a7933db2f77abcd60a5602201
muleyo <admin@muleyo.dev>
2025-08-01 10:36:59 +0200

    Fixed Castbars Icon Border for MOP Classic

--- Modules/Castbars/Icon.lua
+++ Modules/Castbars/Icon.lua
@@ -4,29 +4,57 @@ function Icon:OnInitialize()
     -- Load Database
     Icon.db = mUI.db.profile.general
 
-    C_Timer.After(0, function()
-        if PlayerCastingBarFrame.mUIBorder then
-            PlayerCastingBarFrame.mUIBorder:Hide()
-        end
-    end)
-end
-
-function Icon:OnEnable()
-    PlayerCastingBarFrame.Icon:Show()
-    PlayerCastingBarFrame.Icon:SetSize(20, 20)
-
-    if Icon.db.theme ~= "Disabled" then
-        C_Timer.After(0.1, function()
+    if mUI:IsClassic() then
+        C_Timer.After(0, function()
+            if CastingBarFrame.mUIBorder then
+                CastingBarFrame.mUIBorder:Hide()
+            end
+        end)
+    else
+        C_Timer.After(0, function()
             if PlayerCastingBarFrame.mUIBorder then
-                PlayerCastingBarFrame.mUIBorder:Show()
+                PlayerCastingBarFrame.mUIBorder:Hide()
             end
         end)
     end
 end
 
-function Icon:OnDisable()
-    PlayerCastingBarFrame.Icon:Hide()
-    PlayerCastingBarFrame.Icon:SetSize(16, 16)
+function Icon:OnEnable()
+    if mUI:IsClassic() then
+        CastingBarFrame.Icon:Show()
+        CastingBarFrame.Icon:SetSize(20, 20)
 
-    PlayerCastingBarFrame.mUIBorder:Hide()
+        if Icon.db.theme ~= "Disabled" then
+            C_Timer.After(0.1, function()
+                if CastingBarFrame.mUIBorder then
+                    CastingBarFrame.mUIBorder:Show()
+                end
+            end)
+        end
+    else
+        PlayerCastingBarFrame.Icon:Show()
+        PlayerCastingBarFrame.Icon:SetSize(20, 20)
+
+        if Icon.db.theme ~= "Disabled" then
+            C_Timer.After(0.1, function()
+                if PlayerCastingBarFrame.mUIBorder then
+                    PlayerCastingBarFrame.mUIBorder:Show()
+                end
+            end)
+        end
+    end
+end
+
+function Icon:OnDisable()
+    if mUI:IsClassic() then
+        CastingBarFrame.Icon:Hide()
+        CastingBarFrame.Icon:SetSize(16, 16)
+
+        CastingBarFrame.mUIBorder:Hide()
+    else
+        PlayerCastingBarFrame.Icon:Hide()
+        PlayerCastingBarFrame.Icon:SetSize(16, 16)
+
+        PlayerCastingBarFrame.mUIBorder:Hide()
+    end
 end
