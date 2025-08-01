8b6d08b9a06aa2a56222aaf0d817981b22358bc1
muleyo <admin@muleyo.dev>
2025-08-01 10:34:27 +0200

    Fixed Hide Hotkey for MOP Classic

--- Modules/Actionbars/Hotkey.lua
+++ Modules/Actionbars/Hotkey.lua
@@ -1,28 +1,50 @@
 local Hotkey = mUI:NewModule("mUI.Modules.Actionbars.Hotkey")
 
 function Hotkey:OnInitialize()
-    Hotkey.bars = {
-        MainMenuBar = MainMenuBar,
-        MultiBarBottomLeft = MultiBarBottomLeft,
-        MultiBarBottomRight = MultiBarBottomRight,
-        MultiBarLeft = MultiBarLeft,
-        MultiBarRight = MultiBarRight,
-        MultiBar5 = MultiBar5,
-        MultiBar6 = MultiBar6,
-        MultiBar7 = MultiBar7,
-        PetActionBar = PetActionBar,
-        StanceBar = StanceBar
-    }
+    if mUI:IsClassic() then
+        Hotkey.bars = {
+            MainMenuBar = "MainMenuBar",
+            MultiBarBottomLeft = "MultiBarBottomLeft",
+            MultiBarBottomRight = "MultiBarBottomRight",
+            MultiBarLeft = "MultiBarLeft",
+            MultiBarRight = "MultiBarRight",
+            MultiBar5 = "MultiBar5",
+            MultiBar6 = "MultiBar6",
+            MultiBar7 = "MultiBar7",
+            PetActionBar = "PetActionBar",
+            StanceBar = "StanceBar"
+        }
+    else
+        Hotkey.bars = {
+            MainMenuBar = MainMenuBar,
+            MultiBarBottomLeft = MultiBarBottomLeft,
+            MultiBarBottomRight = MultiBarBottomRight,
+            MultiBarLeft = MultiBarLeft,
+            MultiBarRight = MultiBarRight,
+            MultiBar5 = MultiBar5,
+            MultiBar6 = MultiBar6,
+            MultiBar7 = MultiBar7,
+            PetActionBar = PetActionBar,
+            StanceBar = StanceBar
+        }
+    end
 
     function Hotkey:Update(isEnabled)
         local numButtons
         local hotkey
         for name, bar in pairs(Hotkey.bars) do
             if bar then
-                numButtons = bar.numButtonsShowable
+                if mUI:IsClassic() then
+                    numButtons = 12
+                else
+                    numButtons = bar.numButtonsShowable
+                end
+
                 for i = 1, numButtons do
                     if name == "MainMenuBar" then
                         hotkey = _G["ActionButton" .. i .. "HotKey"]
+                    elseif name == "PetActionBar" then
+                        hotkey = _G["PetActionButton" .. i .. "HotKey"]
                     else
                         hotkey = _G[name .. "Button" .. i .. "HotKey"]
                     end
