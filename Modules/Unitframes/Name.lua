9f50a3a18e470412e5309f53260f04d1eb6ea3f7
muleyo <admin@muleyo.dev>
2025-08-01 10:47:52 +0200

    Fixed Hide Name for MOP Classic

--- Modules/Unitframes/Name.lua
+++ Modules/Unitframes/Name.lua
@@ -1,13 +1,25 @@
 local Name = mUI:NewModule("mUI.Modules.Unitframes.Name")
 
 function Name:OnEnable()
-    PlayerName:SetAlpha(0)
-    TargetFrame.TargetFrameContent.TargetFrameContentMain.Name:SetAlpha(0)
-    FocusFrame.TargetFrameContent.TargetFrameContentMain.Name:SetAlpha(0)
+    if mUI:IsClassic() then
+        PlayerName:SetAlpha(0)
+        TargetFrameTextureFrameName:SetAlpha(0)
+        FocusFrameTextureFrameName:SetAlpha(0)
+    else
+        PlayerName:SetAlpha(0)
+        TargetFrame.TargetFrameContent.TargetFrameContentMain.Name:SetAlpha(0)
+        FocusFrame.TargetFrameContent.TargetFrameContentMain.Name:SetAlpha(0)
+    end
 end
 
 function Name:OnDisable()
-    PlayerName:SetAlpha(1)
-    TargetFrame.TargetFrameContent.TargetFrameContentMain.Name:SetAlpha(1)
-    FocusFrame.TargetFrameContent.TargetFrameContentMain.Name:SetAlpha(1)
+    if mUI:IsClassic() then
+        PlayerName:SetAlpha(1)
+        TargetFrameTextureFrameName:SetAlpha(1)
+        FocusFrameTextureFrameName:SetAlpha(1)
+    else
+        PlayerName:SetAlpha(1)
+        TargetFrame.TargetFrameContent.TargetFrameContentMain.Name:SetAlpha(1)
+        FocusFrame.TargetFrameContent.TargetFrameContentMain.Name:SetAlpha(1)
+    end
 end
