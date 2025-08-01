5cd3c9ec2d9744ad0e1c4a0929135f37eaee94a0
muleyo <admin@muleyo.dev>
2025-08-01 10:42:09 +0200

    Disabled Dragonflying Module for MOP Classic

--- Modules/Misc/Dragonflying.lua
+++ Modules/Misc/Dragonflying.lua
@@ -3,14 +3,16 @@ local Dragonflying = mUI:NewModule("mUI.Modules.Misc.Dragonflying", "AceHook-3.0
 function Dragonflying:OnInitialize()
     Dragonflying.frame = CreateFrame("Frame")
 
-    function Dragonflying:Update(show)
-        for _, child in ipairs({ UIWidgetPowerBarContainerFrame:GetChildren() }) do
-            for _, v in ipairs({ child:GetRegions() }) do
-                if v:GetObjectType() == "Texture" then
-                    if show then
-                        v:Show()
-                    else
-                        v:Hide()
+    if not mUI:IsClassic() then
+        function Dragonflying:Update(show)
+            for _, child in ipairs({ UIWidgetPowerBarContainerFrame:GetChildren() }) do
+                for _, v in ipairs({ child:GetRegions() }) do
+                    if v:GetObjectType() == "Texture" then
+                        if show then
+                            v:Show()
+                        else
+                            v:Hide()
+                        end
                     end
                 end
             end
@@ -19,13 +21,17 @@ function Dragonflying:OnInitialize()
 end
 
 function Dragonflying:OnEnable()
-    Dragonflying.frame:RegisterEvent("UPDATE_UI_WIDGET")
-    Dragonflying:HookScript(Dragonflying.frame, "OnEvent", function()
-        Dragonflying:Update()
-    end)
+    if not mUI:IsClassic() then
+        Dragonflying.frame:RegisterEvent("UPDATE_UI_WIDGET")
+        Dragonflying:SecureHookScript(Dragonflying.frame, "OnEvent", function()
+            Dragonflying:Update()
+        end)
+    end
 end
 
 function Dragonflying:OnDisable()
-    Dragonflying:Update(true)
-    Dragonflying:UnhookAll()
+    if not mUI:IsClassic() then
+        Dragonflying:Update(true)
+        Dragonflying:UnhookAll()
+    end
 end
