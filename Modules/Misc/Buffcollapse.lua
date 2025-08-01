37fc9f8b8a56a02cf828fd55f34be34d124b0495
muleyo <admin@muleyo.dev>
2025-08-01 10:41:55 +0200

    Disabled Buff Collapse Option for MOP Classic

--- Modules/Misc/Buffcollapse.lua
+++ Modules/Misc/Buffcollapse.lua
@@ -1,15 +1,22 @@
 local Buffcollapse = mUI:NewModule("mUI.Modules.Misc.Buffcollapse", "AceHook-3.0")
 
 function Buffcollapse:OnInitialize()
-    Buffcollapse.func = BuffFrame.CollapseAndExpandButton.Show
+    if not mUI:IsClassic() then
+        Buffcollapse.func = BuffFrame.CollapseAndExpandButton.Show
+    end
 end
 
 function Buffcollapse:OnEnable()
-    BuffFrame.CollapseAndExpandButton:Hide()
-    Buffcollapse:HookScript(BuffFrame.CollapseAndExpandButton, "OnShow", BuffFrame.CollapseAndExpandButton.Hide)
+    if not mUI:IsClassic() then
+        BuffFrame.CollapseAndExpandButton:Hide()
+        Buffcollapse:SecureHookScript(BuffFrame.CollapseAndExpandButton, "OnShow", BuffFrame.CollapseAndExpandButton
+        .Hide)
+    end
 end
 
 function Buffcollapse:OnDisable()
-    Buffcollapse:UnhookAll()
-    BuffFrame.CollapseAndExpandButton:Show()
+    if not mUI:IsClassic() then
+        Buffcollapse:UnhookAll()
+        BuffFrame.CollapseAndExpandButton:Show()
+    end
 end
