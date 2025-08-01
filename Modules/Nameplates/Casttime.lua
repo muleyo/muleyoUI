e7d3ba4fdae91606089156c0d4f43041db3ed698
muleyo <admin@muleyo.dev>
2025-08-01 10:43:58 +0200

    Fixed Casttime on Nameplates for MOP Classic

--- Modules/Nameplates/Casttime.lua
+++ Modules/Nameplates/Casttime.lua
@@ -18,7 +18,13 @@ function Casttime:OnInitialize()
 
         -- Create Border
         parent.mUIBorder = parent:CreateTexture()
-        parent.mUIBorder:SetAtlas("UI-HUD-ActionBar-IconFrame")
+        if mUI:IsClassic() then
+            parent.mUIBorder:SetTexture([[Interface\AddOns\mUI\Media\Textures\Core\uiactionbar2x.png]])
+            parent.mUIBorder:SetTexCoord(0.701171875, 0.880859375, 0.31689453125, 0.36083984375)
+        else
+            parent.mUIBorder:SetAtlas("UI-HUD-ActionBar-IconFrame")
+        end
+
         parent.mUIBorder:SetVertexColor(unpack(mUI:Color(0.25)))
 
         -- Set Icon Mask
@@ -29,34 +35,52 @@ function Casttime:OnInitialize()
         parent.mUIBorder:SetDrawLayer("OVERLAY", 1)
         icon:AddMaskTexture(parent.mUIBorder.mask)
 
-        parent.mUIBorder:SetPoint("TOPLEFT", icon, "TOPLEFT", -0.75, 0.75)
-        parent.mUIBorder:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 1.8, -0.75)
+        if mUI:IsClassic() then
+            parent.mUIBorder:SetPoint("TOPLEFT", icon, "TOPLEFT", -0.75, 0.75)
+            parent.mUIBorder:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 1, -0.75)
+        else
+            parent.mUIBorder:SetPoint("TOPLEFT", icon, "TOPLEFT", -0.75, 0.75)
+            parent.mUIBorder:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 1.8, -0.75)
+        end
     end
 
     function Casttime:NameplateCastbar(frame)
         if not frame or frame:IsForbidden() then return end
         if frame.unit and frame.unit:find('nameplate%d') then
-            local _, _, _, _, _, _, _, castInterrupt = UnitCastingInfo(frame.unit);
-            local _, _, _, _, _, _, channelInterrupt, _, _, _ = UnitChannelInfo(frame.unit);
+            local _, _, _, _, _, _, _, castInterrupt = UnitCastingInfo(frame.unit)
+            local _, _, _, _, _, _, channelInterrupt, _, _, _ = UnitChannelInfo(frame.unit)
 
             if frame and frame.Icon then
-                if frame.BorderShield then
-                    frame.BorderShield:ClearAllPoints()
-                    frame.BorderShield:SetAtlas("ui-castingbar-shield")
-                    PixelUtil.SetPoint(frame.BorderShield, "CENTER", frame, "LEFT", -10, 0)
+                if not mUI:IsClassic() then
+                    if frame.BorderShield then
+                        frame.BorderShield:ClearAllPoints()
+                        frame.BorderShield:SetAtlas("ui-castingbar-shield")
+                        PixelUtil.SetPoint(frame.BorderShield, "CENTER", frame, "LEFT", -10, 0)
+                    end
                 end
 
-                frame.Icon:ClearAllPoints();
-                PixelUtil.SetPoint(frame.Icon, "CENTER", frame, "LEFT", -10, 0)
+                if mUI:IsClassic() then
+                    frame:SetStatusBarTexture(Casttime.LSM:Fetch('statusbar', Casttime.db.nameplates.texture))
+                    frame:GetStatusBarTexture():SetDrawLayer("BORDER")
+                end
+
+                frame.Icon:ClearAllPoints()
+                if mUI:IsClassic() then
+                    PixelUtil.SetPoint(frame.Icon, "CENTER", frame, "LEFT", -11.25, 2.5)
+                else
+                    PixelUtil.SetPoint(frame.Icon, "CENTER", frame, "LEFT", -10, 0)
+                end
                 frame.Text:SetFont(Casttime.font, 10, "OUTLINE")
                 Casttime:IconSkin(frame.Icon, frame)
 
-                if castInterrupt or channelInterrupt then
-                    frame.Icon:Hide()
-                    frame.mUIBorder:Hide()
-                else
-                    frame.Icon:Show()
-                    frame.mUIBorder:Show()
+                if not mUI:IsClassic() then
+                    if castInterrupt or channelInterrupt then
+                        frame.Icon:Hide()
+                        frame.mUIBorder:Hide()
+                    else
+                        frame.Icon:Show()
+                        frame.mUIBorder:Show()
+                    end
                 end
 
                 if Casttime.db.nameplates.casttime then
@@ -92,20 +116,27 @@ function Casttime:OnInitialize()
                 PixelUtil.SetPoint(frame.castBar.BorderShield, "CENTER", frame.castBar, "LEFT", -10, 0)
             end
 
-            frame.castBar.Icon:ClearAllPoints();
-            PixelUtil.SetPoint(frame.castBar.Icon, "CENTER", frame.castBar, "LEFT", -10, 0);
+            frame.castBar.Icon:ClearAllPoints()
+            PixelUtil.SetPoint(frame.castBar.Icon, "CENTER", frame.castBar, "LEFT", -10, 0)
             frame.castBar.Text:SetFont(Casttime.font, 10, "OUTLINE")
         end
     end
 end
 
 function Casttime:OnEnable()
-    Casttime:SecureHook(CastingBarMixin, "OnUpdate", function(frame)
-        Casttime:NameplateCastbar(frame)
-    end)
-    Casttime:SecureHook("DefaultCompactNamePlateFrameAnchorInternal", function(frame)
-        Casttime:NameplateCastbarIcon(frame)
-    end)
+    if mUI:IsClassic() then
+        Casttime:SecureHook("CastingBarFrame_OnUpdate", function(frame)
+            Casttime:NameplateCastbar(frame)
+        end)
+    else
+        Casttime:SecureHook(CastingBarMixin, "OnUpdate", function(frame)
+            Casttime:NameplateCastbar(frame)
+        end)
+
+        Casttime:SecureHook("DefaultCompactNamePlateFrameAnchorInternal", function(frame)
+            Casttime:NameplateCastbarIcon(frame)
+        end)
+    end
 end
 
 function Casttime:OnDisable()
