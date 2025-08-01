7aba01fb9309e031b0a61ae640c8a4562792b14c
muleyo <admin@muleyo.dev>
2025-08-01 10:49:10 +0200

    Fixed Hit Indicator for MOP Classic

--- Modules/Unitframes/Hitindicator.lua
+++ Modules/Unitframes/Hitindicator.lua
@@ -3,11 +3,23 @@ local Hitindicator = mUI:NewModule("mUI.Modules.Unitframes.Hitindicator")
 function Hitindicator:OnInitialize()
     -- Backup original function
     Hitindicator.pet = PetHitIndicator.SetText
+
+    if mUI:IsClassic() then
+        Hitindicator.player = PlayerHitIndicator.SetText
+    else
+        Hitindicator.player = PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator.SetText
+    end
 end
 
 function Hitindicator:OnEnable()
     -- Hide PlayerFrame Hit Indicator
-    PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator:Hide()
+    if mUI:IsClassic() then
+        PlayerHitIndicator:SetText(nil)
+        PlayerHitIndicator.SetText = function() end
+    else
+        PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator:SetText(nil)
+        PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator.SetText = function() end
+    end
 
     -- Hide PetFrame Hit Indicator
     PetHitIndicator:SetText(nil)
@@ -16,6 +28,11 @@ end
 
 function Hitindicator:OnDisable()
     -- Restore functionality
-    PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator:Show()
+    if mUI:IsClassic() then
+        PlayerHitIndicator.SetText = Hitindicator.player
+    else
+        PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator.SetText = Hitindicator.player
+    end
+
     PetHitIndicator.SetText = Hitindicator.pet
 end
