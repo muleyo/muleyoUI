811a290e45b33a99d979a9b4a58cc3195266e2d9
muleyo <admin@muleyo.dev>
2025-08-01 10:49:18 +0200

    Disabled Corner Icon Option for MOP Classic

--- Modules/Unitframes/Cornericon.lua
+++ Modules/Unitframes/Cornericon.lua
@@ -1,11 +1,15 @@
 local Cornericon = mUI:NewModule("mUI.Modules.Unitframes.Cornericon")
 
 function Cornericon:OnEnable()
-    C_Timer.After(0, function()
-        PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.PlayerPortraitCornerIcon:SetAlpha(0)
-    end)
+    if not mUI:IsClassic() then
+        C_Timer.After(0, function()
+            PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.PlayerPortraitCornerIcon:SetAlpha(0)
+        end)
+    end
 end
 
 function Cornericon:OnDisable()
-    PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.PlayerPortraitCornerIcon:SetAlpha(1)
+    if not mUI:IsClassic() then
+        PlayerFrame.PlayerFrameContent.PlayerFrameContentContextual.PlayerPortraitCornerIcon:SetAlpha(1)
+    end
 end
