863083176236956a53b70f16361cc9deb6b432e6
muleyo <admin@muleyo.dev>
2025-08-01 10:40:59 +0200

    Fixed Classbar Theme for MOP Classic

--- Modules/General/Theme/ClassBar.lua
+++ Modules/General/Theme/ClassBar.lua
@@ -4,130 +4,191 @@ local _, playerClass = UnitClass("player")
 function Theme:ClassBar()
     -- Rogue
     if (playerClass == "ROGUE") then
-        for _, child in pairs({ RogueComboPointBarFrame:GetChildren() }) do
-            mUI:Skin({
-                child.BGActive,
-                child.BGInactive,
-                child.BGShadow
-            }, true)
-            if (child.isCharged) then
+        if not mUI:IsClassic() then
+            for _, child in pairs({ RogueComboPointBarFrame:GetChildren() }) do
                 mUI:Skin({
-                    child.ChargedFrameActive
+                    child.BGActive,
+                    child.BGInactive,
+                    child.BGShadow
                 }, true)
+                if (child.isCharged) then
+                    mUI:Skin({
+                        child.ChargedFrameActive
+                    }, true)
+                end
             end
-        end
-        for _, child in pairs({ ClassNameplateBarRogueFrame:GetChildren() }) do
-            mUI:Skin({
-                child.BGActive,
-                child.BGInactive,
-                child.BGShadow
-            }, true)
-            if (child.isCharged) then
+            for _, child in pairs({ ClassNameplateBarRogueFrame:GetChildren() }) do
                 mUI:Skin({
-                    child.ChargedFrameActive
+                    child.BGActive,
+                    child.BGInactive,
+                    child.BGShadow
                 }, true)
+                if (child.isCharged) then
+                    mUI:Skin({
+                        child.ChargedFrameActive
+                    }, true)
+                end
             end
         end
     elseif (playerClass == "MAGE") then
         -- Mage
-        for _, child in pairs({ MageArcaneChargesFrame:GetChildren() }) do
-            mUI:Skin({
-                child.ArcaneBG,
-                child.ArcaneBGShadow
-            }, true)
-        end
-        for _, child in pairs({ ClassNameplateBarMageFrame:GetChildren() }) do
-            mUI:Skin({
-                child.ArcaneBG,
-                child.ArcaneBGShadow
-            }, true)
+        if not mUI:IsClassic() then
+            for _, child in pairs({ MageArcaneChargesFrame:GetChildren() }) do
+                mUI:Skin({
+                    child.ArcaneBG,
+                    child.ArcaneBGShadow
+                }, true)
+            end
+            for _, child in pairs({ ClassNameplateBarMageFrame:GetChildren() }) do
+                mUI:Skin({
+                    child.ArcaneBG,
+                    child.ArcaneBGShadow
+                }, true)
+            end
         end
     elseif (playerClass == "WARLOCK") then
         -- Warlock
-        for _, child in pairs({ WarlockPowerFrame:GetChildren() }) do
+        if mUI:IsClassic() then
+            -- Destruction Warlock
             mUI:Skin({
-                child.Background
+                select(1, BurningEmbersBarFrameEmber1:GetRegions()),
+                select(1, BurningEmbersBarFrameEmber2:GetRegions()),
+                select(1, BurningEmbersBarFrameEmber3:GetRegions()),
+                select(1, BurningEmbersBarFrameEmber4:GetRegions()),
+                BurningEmbersBarFrame.background
             }, true)
-        end
-        for _, child in pairs({ ClassNameplateBarWarlockFrame:GetChildren() }) do
+
+            -- Affliction
             mUI:Skin({
-                child.Background
+                select(1, select(5, ShardBarFrameShard1:GetRegions())),
+                select(1, select(5, ShardBarFrameShard2:GetRegions())),
+                select(1, select(5, ShardBarFrameShard3:GetRegions())),
+                select(1, select(5, ShardBarFrameShard4:GetRegions()))
             }, true)
+
+            -- Demonology
+            mUI:Skin({
+                select(3, DemonicFuryBarFrame:GetRegions())
+            }, true)
+        else
+            for _, child in pairs({ WarlockPowerFrame:GetChildren() }) do
+                mUI:Skin({
+                    child.Background
+                }, true)
+            end
+            for _, child in pairs({ ClassNameplateBarWarlockFrame:GetChildren() }) do
+                mUI:Skin({
+                    child.Background
+                }, true)
+            end
         end
     elseif (playerClass == "DRUID") then
         -- Druid
-        for _, child in pairs({ DruidComboPointBarFrame:GetChildren() }) do
-            mUI:Skin({
-                child.BG_Active,
-                child.BG_Inactive,
-                child.BG_Shadow
-            }, true)
-        end
-        for _, child in pairs({ ClassNameplateBarFeralDruidFrame:GetChildren() }) do
-            mUI:Skin({
-                child.BG_Active,
-                child.BG_Inactive,
-                child.BG_Shadow
-            }, true)
+        if not mUI:IsClassic() then
+            for _, child in pairs({ DruidComboPointBarFrame:GetChildren() }) do
+                mUI:Skin({
+                    child.BG_Active,
+                    child.BG_Inactive,
+                    child.BG_Shadow
+                }, true)
+            end
+            for _, child in pairs({ ClassNameplateBarFeralDruidFrame:GetChildren() }) do
+                mUI:Skin({
+                    child.BG_Active,
+                    child.BG_Inactive,
+                    child.BG_Shadow
+                }, true)
+            end
         end
     elseif (playerClass == "MONK") then
         -- Monk
-        for _, child in pairs({ MonkHarmonyBarFrame:GetChildren() }) do
+        if mUI:IsClassic() then
             mUI:Skin({
-                child.Chi_BG,
-                child.Chi_BG_Active
+                select(2, MonkHarmonyBar:GetRegions()),
+                select(6, MonkStaggerBar:GetRegions())
             }, true)
-        end
-        for _, child in pairs({ ClassNameplateBarWindwalkerMonkFrame:GetChildren() }) do
-            mUI:Skin({
-                child.Chi_BG,
-                child.Chi_BG_Active
-            }, true)
+        else
+            for _, child in pairs({ MonkHarmonyBarFrame:GetChildren() }) do
+                mUI:Skin({
+                    child.Chi_BG,
+                    child.Chi_BG_Active
+                }, true)
+            end
+            for _, child in pairs({ ClassNameplateBarWindwalkerMonkFrame:GetChildren() }) do
+                mUI:Skin({
+                    child.Chi_BG,
+                    child.Chi_BG_Active
+                }, true)
+            end
         end
     elseif (playerClass == "DEATHKNIGHT") then
         -- Death Knight
-        for _, child in pairs({ RuneFrame:GetChildren() }) do
+        if mUI:IsClassic() then
             mUI:Skin({
-                child.BG_Active,
-                child.BG_Inactive,
-                child.BG_Shadow
+                Rune1BorderTexture,
+                Rune2BorderTexture,
+                Rune3BorderTexture,
+                Rune4BorderTexture,
+                Rune5BorderTexture,
+                Rune6BorderTexture
             }, true)
-        end
-        for _, child in pairs({ DeathKnightResourceOverlayFrame:GetChildren() }) do
-            mUI:Skin({
-                child.BG_Active,
-                child.BG_Inactive,
-                child.BG_Shadow
-            }, true)
+        else
+            for _, child in pairs({ RuneFrame:GetChildren() }) do
+                mUI:Skin({
+                    child.BG_Active,
+                    child.BG_Inactive,
+                    child.BG_Shadow
+                }, true)
+            end
+            for _, child in pairs({ DeathKnightResourceOverlayFrame:GetChildren() }) do
+                mUI:Skin({
+                    child.BG_Active,
+                    child.BG_Inactive,
+                    child.BG_Shadow
+                }, true)
+            end
         end
     elseif (playerClass == "EVOKER") then
         -- Evoker
-        for _, child in pairs({ EssencePlayerFrame:GetChildren() }) do
-            if not child.EssenceFillDone then return end
-            mUI:Skin({
-                child.EssenceFillDone.CircBG,
-                child.EssenceFillDone.CircBGActive
-            }, true)
-        end
+        if not mUI:IsClassic() then
+            for _, child in pairs({ EssencePlayerFrame:GetChildren() }) do
+                if not child.EssenceFillDone then return end
+                mUI:Skin({
+                    child.EssenceFillDone.CircBG,
+                    child.EssenceFillDone.CircBGActive
+                }, true)
+            end
 
-        for _, child in pairs({ ClassNameplateBarDracthyrFrame:GetChildren() }) do
-            mUI:Skin({
-                child.EssenceFillDone.CircBG,
-                child.EssenceFillDone.CircBGActive
-            }, true)
+            for _, child in pairs({ ClassNameplateBarDracthyrFrame:GetChildren() }) do
+                mUI:Skin({
+                    child.EssenceFillDone.CircBG,
+                    child.EssenceFillDone.CircBGActive
+                }, true)
+            end
         end
     elseif (playerClass == "PALADIN") then
         -- Paladin
-        mUI:Skin({
-            PaladinPowerBarFrame.Background,
-            PaladinPowerBarFrame.ActiveTexture,
-            ClassNameplateBarPaladinFrame.Background,
-            ClassNameplateBarPaladinFrame.ActiveTexture
-        }, true)
+        if mUI:IsClassic() then
+            mUI:Skin(PaladinPowerBar)
+        else
+            mUI:Skin({
+                PaladinPowerBarFrame.Background,
+                PaladinPowerBarFrame.ActiveTexture,
+                ClassNameplateBarPaladinFrame.Background,
+                ClassNameplateBarPaladinFrame.ActiveTexture
+            }, true)
+        end
+    elseif (playerClass == "PRIEST") then
+        if mUI:IsClassic() then
+            mUI:Skin({ select(1, PriestBarFrame:GetRegions()) }, true)
+        end
     end
-    if (playerClass == "SHAMAN" or playerClass == "PALADIN" or playerClass == "PRIEST" or playerClass == "DRUID") then
-        for totem, _ in TotemFrame.totemPool:EnumerateActive() do
-            mUI:Skin({ totem.Border }, true)
+    if (playerClass == "SHAMAN" or playerClass == "PALADIN" or (playerClass == "PRIEST" and not mUI:IsClassic()) or playerClass == "DRUID") then
+        if mUI:IsClassic() then
+        else
+            for totem, _ in TotemFrame.totemPool:EnumerateActive() do
+                mUI:Skin({ totem.Border }, true)
+            end
         end
     end
 end
