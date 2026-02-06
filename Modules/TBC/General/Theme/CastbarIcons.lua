bd9c09a21b09b4522729d51e93e25529867fbb84
muleyo <admin@muleyo.dev>
2026-02-06 21:25:16 +0100

    Fixed FocusFrame Castbars

--- Modules/TBC/General/Theme/CastbarIcons.lua
+++ Modules/TBC/General/Theme/CastbarIcons.lua
@@ -3,6 +3,7 @@ local Theme = mUI:GetModule("mUI.Modules.General.Theme")
 Theme.castbars = {
     player = "PlayerCastingBarFrame",
     target = "TargetFrameSpellBar",
+    focus = "FocusFrameSpellBar",
     boss1 = "Boss1TargetFrameSpellBar",
     boss2 = "Boss2TargetFrameSpellBar",
     boss3 = "Boss3TargetFrameSpellBar",
