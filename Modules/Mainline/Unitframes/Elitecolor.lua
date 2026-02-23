83c4b4e0b0a5449f6b97c9c14106048b0c17ae2b
muleyo <admin@muleyo.dev>
2026-02-23 09:22:36 +0100

    Elitechain now also works for Focus and Boss frames

--- Modules/Mainline/Unitframes/Elitecolor.lua
+++ Modules/Mainline/Unitframes/Elitecolor.lua
@@ -1,17 +1,31 @@
 local Elitecolor = mUI:NewModule("mUI.Modules.Unitframes.Elitecolor")
 
 function Elitecolor:OnEnable()
+    Elitecolor.frames = {
+        target = TargetFrame.TargetFrameContainer,
+        focus = FocusFrame.TargetFrameContainer,
+        boss1 = Boss1TargetFrame.TargetFrameContainer,
+        boss2 = Boss2TargetFrame.TargetFrameContainer,
+        boss3 = Boss3TargetFrame.TargetFrameContainer,
+        boss4 = Boss4TargetFrame.TargetFrameContainer,
+        boss5 = Boss5TargetFrame.TargetFrameContainer
+    }
+
     C_Timer.After(0.1, function()
-        if select(5, TargetFrame.TargetFrameContainer:GetRegions()) then
-            select(5, TargetFrame.TargetFrameContainer:GetRegions()):SetDesaturated(false)
-            select(5, TargetFrame.TargetFrameContainer:GetRegions()):SetVertexColor(1, 1, 1)
+        for _, frame in pairs(Elitecolor.frames) do
+            if select(5, frame:GetRegions()) then
+                select(5, frame:GetRegions()):SetDesaturated(false)
+                select(5, frame:GetRegions()):SetVertexColor(1, 1, 1)
+            end
         end
     end)
 end
 
 function Elitecolor:OnDisable()
-    if select(5, TargetFrame.TargetFrameContainer:GetRegions()) then
-        select(5, TargetFrame.TargetFrameContainer:GetRegions()):SetDesaturated(true)
-        select(5, TargetFrame.TargetFrameContainer:GetRegions()):SetVertexColor(unpack(mUI:Color(0.15)))
+    for _, frame in pairs(Elitecolor.frames) do
+        if select(5, frame:GetRegions()) then
+            select(5, frame:GetRegions()):SetDesaturated(true)
+            select(5, frame:GetRegions()):SetVertexColor(unpack(mUI:Color(0.15)))
+        end
     end
 end
