8c473686591441d3ea34b2b38e8b0ebc497a87d0
muleyo <admin@muleyo.dev>
2026-02-27 13:20:13 +0100

    Fixed automation quests error

--- Modules/Mainline/General/Quests.lua
+++ Modules/Mainline/General/Quests.lua
@@ -3,13 +3,20 @@ local Quests = mUI:NewModule("mUI.Modules.General.Quests", "AceHook-3.0")
 function Quests:OnInitialize()
     function Quests:Accept()
         QuestFrameAcceptButton:Click()
-        QuestFrameCompleteQuestButton:Click()
+    end
+
+    function Quests:Complete()
+        QuestFrameCompleteButton:Click()
+        if GetNumQuestChoices() <= 1 then
+            QuestFrameCompleteQuestButton:Click()
+        end
     end
 end
 
 function Quests:OnEnable()
     Quests:SecureHookScript(QuestFrameAcceptButton, "OnShow", Quests.Accept)
-    Quests:SecureHookScript(QuestFrameCompleteQuestButton, "OnShow", Quests.Accept)
+    Quests:SecureHookScript(QuestFrameCompleteQuestButton, "OnShow", Quests.Complete)
+    Quests:SecureHookScript(QuestFrameCompleteButton, "OnShow", Quests.Complete)
 end
 
 function Quests:OnDisable()
