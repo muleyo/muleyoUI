ae809d3538b3759dc4431bd818f41dc7132699cf
muleyo <admin@muleyo.dev>
2026-03-14 22:50:06 +0100

    Petframe:HideBase() Taint fix

--- Modules/Mainline/Unitframes/Hitindicator.lua
+++ Modules/Mainline/Unitframes/Hitindicator.lua
@@ -1,25 +1,17 @@
-local Hitindicator = mUI:NewModule("mUI.Modules.Unitframes.Hitindicator")
+local Hitindicator = mUI:NewModule("mUI.Modules.Unitframes.Hitindicator", "AceHook-3.0")
 
 function Hitindicator:OnInitialize()
-    -- Backup original function
-    Hitindicator.pet = PetHitIndicator.SetText
-    Hitindicator.player = PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator.HitText.SetText
+    local PlayerHitIndicator = PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator.HitText
+    function Hitindicator:Update()
+        PlayerHitIndicator:Hide()
+        PetHitIndicator:Hide()
+    end
 end
 
 function Hitindicator:OnEnable()
-    -- Hide PlayerFrame Hit Indicator
-    PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator.HitText:SetText(nil)
-    PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator.HitText.SetText = function()
-    end
-
-    -- Hide PetFrame Hit Indicator
-    PetHitIndicator:SetText(nil)
-    PetHitIndicator.SetText = function()
-    end
+    Hitindicator:SecureHook("CombatFeedback_OnCombatEvent", Hitindicator.Update)
 end
 
 function Hitindicator:OnDisable()
-    -- Restore functionality
-    PlayerFrame.PlayerFrameContent.PlayerFrameContentMain.HitIndicator.HitText.SetText = Hitindicator.player
-    PetHitIndicator.SetText = Hitindicator.pet
+    Hitindicator:UnhookAll()
 end
