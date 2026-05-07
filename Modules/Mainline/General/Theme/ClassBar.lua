e4b322c767c536c75c2544d3622ba771b4632c39
muleyo <admin@muleyo.dev>
2026-05-07 15:29:55 +0200

    Updated PersonalResourceDisplay Theme for patch 12.0.7

--- Modules/Mainline/General/Theme/ClassBar.lua
+++ Modules/Mainline/General/Theme/ClassBar.lua
@@ -10,10 +10,20 @@ function Theme:ClassBar()
                 mUI:Skin({child.ChargedFrameActive}, true)
             end
         end
-        for _, child in pairs({prdClassFrame:GetChildren()}) do
-            mUI:Skin({child.BGActive, child.BGInactive, child.BGShadow}, true)
-            if (child.isCharged) then
-                mUI:Skin({child.ChargedFrameActive}, true)
+
+        if PersonalResourceDisplayFrame and PersonalResourceDisplayFrame.ClassFrameContainer then
+            for _, child in pairs({PersonalResourceDisplayFrame.ClassFrameContainer:GetChildren()}) do
+                mUI:Skin({child.BGActive, child.BGInactive, child.BGShadow}, true)
+                if (child.isCharged) then
+                    mUI:Skin({child.ChargedFrameActive}, true)
+                end
+            end
+        else
+            for _, child in pairs({prdClassFrame:GetChildren()}) do
+                mUI:Skin({child.BGActive, child.BGInactive, child.BGShadow}, true)
+                if (child.isCharged) then
+                    mUI:Skin({child.ChargedFrameActive}, true)
+                end
             end
         end
     elseif (playerClass == "MAGE") then
@@ -21,40 +31,75 @@ function Theme:ClassBar()
         for _, child in pairs({MageArcaneChargesFrame:GetChildren()}) do
             mUI:Skin({child.ArcaneBG, child.ArcaneBGShadow}, true)
         end
-        for _, child in pairs({prdClassFrame:GetChildren()}) do
-            mUI:Skin({child.ArcaneBG, child.ArcaneBGShadow}, true)
+
+        if PersonalResourceDisplayFrame and PersonalResourceDisplayFrame.ClassFrameContainer then
+            for _, child in pairs({PersonalResourceDisplayFrame.ClassFrameContainer:GetChildren()}) do
+                mUI:Skin({child.ArcaneBG, child.ArcaneBGShadow}, true)
+            end
+        else
+            for _, child in pairs({prdClassFrame:GetChildren()}) do
+                mUI:Skin({child.ArcaneBG, child.ArcaneBGShadow}, true)
+            end
         end
     elseif (playerClass == "WARLOCK") then
         -- Warlock
         for _, child in pairs({WarlockPowerFrame:GetChildren()}) do
             mUI:Skin({child.Background}, true)
         end
-        for _, child in pairs({prdClassFrame:GetChildren()}) do
-            mUI:Skin({child.Background}, true)
+
+        if PersonalResourceDisplayFrame and PersonalResourceDisplayFrame.ClassFrameContainer then
+            for _, child in pairs({PersonalResourceDisplayFrame.ClassFrameContainer:GetChildren()}) do
+                mUI:Skin({child.Background}, true)
+            end
+        else
+            for _, child in pairs({prdClassFrame:GetChildren()}) do
+                mUI:Skin({child.Background}, true)
+            end
         end
     elseif (playerClass == "DRUID") then
         -- Druid
         for _, child in pairs({DruidComboPointBarFrame:GetChildren()}) do
             mUI:Skin({child.BG_Active, child.BG_Inactive, child.BG_Shadow}, true)
         end
-        for _, child in pairs({prdClassFrame:GetChildren()}) do
-            mUI:Skin({child.BG_Active, child.BG_Inactive, child.BG_Shadow}, true)
+
+        if PersonalResourceDisplayFrame and PersonalResourceDisplayFrame.ClassFrameContainer then
+            for _, child in pairs({PersonalResourceDisplayFrame.ClassFrameContainer:GetChildren()}) do
+                mUI:Skin({child.BG_Active, child.BG_Inactive, child.BG_Shadow}, true)
+            end
+        else
+            for _, child in pairs({prdClassFrame:GetChildren()}) do
+                mUI:Skin({child.BG_Active, child.BG_Inactive, child.BG_Shadow}, true)
+            end
         end
     elseif (playerClass == "MONK") then
         -- Monk
         for _, child in pairs({MonkHarmonyBarFrame:GetChildren()}) do
             mUI:Skin({child.Chi_BG, child.Chi_BG_Active}, true)
         end
-        for _, child in pairs({prdClassFrame:GetChildren()}) do
-            mUI:Skin({child.Chi_BG, child.Chi_BG_Active}, true)
+
+        if PersonalResourceDisplayFrame and PersonalResourceDisplayFrame.ClassFrameContainer then
+            for _, child in pairs({PersonalResourceDisplayFrame.ClassFrameContainer:GetChildren()}) do
+                mUI:Skin({child.Chi_BG, child.Chi_BG_Active}, true)
+            end
+        else
+            for _, child in pairs({prdClassFrame:GetChildren()}) do
+                mUI:Skin({child.Chi_BG, child.Chi_BG_Active}, true)
+            end
         end
     elseif (playerClass == "DEATHKNIGHT") then
         -- Death Knight
         for _, child in pairs({RuneFrame:GetChildren()}) do
             mUI:Skin({child.BG_Active, child.BG_Inactive, child.BG_Shadow}, true)
         end
-        for _, child in pairs({prdClassFrame:GetChildren()}) do
-            mUI:Skin({child.BG_Active, child.BG_Inactive, child.BG_Shadow}, true)
+
+        if PersonalResourceDisplayFrame and PersonalResourceDisplayFrame.ClassFrameContainer then
+            for _, child in pairs({PersonalResourceDisplayFrame.ClassFrameContainer:GetChildren()}) do
+                mUI:Skin({child.BG_Active, child.BG_Inactive, child.BG_Shadow}, true)
+            end
+        else
+            for _, child in pairs({prdClassFrame:GetChildren()}) do
+                mUI:Skin({child.BG_Active, child.BG_Inactive, child.BG_Shadow}, true)
+            end
         end
     elseif (playerClass == "EVOKER") then
         -- Evoker
@@ -65,12 +110,26 @@ function Theme:ClassBar()
             mUI:Skin({child.EssenceFillDone.CircBG, child.EssenceFillDone.CircBGActive}, true)
         end
 
-        for _, child in pairs({prdClassFrame:GetChildren()}) do
-            mUI:Skin({child.EssenceFillDone.CircBG, child.EssenceFillDone.CircBGActive}, true)
+        if PersonalResourceDisplayFrame and PersonalResourceDisplayFrame.ClassFrameContainer then
+            for _, child in pairs({PersonalResourceDisplayFrame.ClassFrameContainer:GetChildren()}) do
+                mUI:Skin({child.EssenceFillDone.CircBG, child.EssenceFillDone.CircBGActive}, true)
+            end
+        else
+            for _, child in pairs({prdClassFrame:GetChildren()}) do
+                mUI:Skin({child.EssenceFillDone.CircBG, child.EssenceFillDone.CircBGActive}, true)
+            end
         end
     elseif (playerClass == "PALADIN") then
         -- Paladin
-        mUI:Skin({PaladinPowerBarFrame.Background, PaladinPowerBarFrame.ActiveTexture, prdClassFrame.Background, prdClassFrame.ActiveTexture}, true)
+        if PersonalResourceDisplayFrame and PersonalResourceDisplayFrame.ClassFrameContainer then
+            for _, child in pairs({PersonalResourceDisplayFrame.ClassFrameContainer:GetChildren()}) do
+                mUI:Skin({child.Background, child.ActiveTexture}, true)
+            end
+            mUI:Skin({PaladinPowerBarFrame.Background, PaladinPowerBarFrame.ActiveTexture}, true)
+        else
+            mUI:Skin({PaladinPowerBarFrame.Background, PaladinPowerBarFrame.ActiveTexture, prdClassFrame.Background, prdClassFrame.ActiveTexture},
+                true)
+        end
     end
     if (playerClass == "SHAMAN" or playerClass == "PALADIN" or playerClass == "PRIEST" or playerClass == "DRUID") then
         for totem, _ in TotemFrame.totemPool:EnumerateActive() do
